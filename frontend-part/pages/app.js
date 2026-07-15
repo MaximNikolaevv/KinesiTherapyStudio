7e2879ccfcdb6c74164bb63505dce93cebe571bc
MaximNikolaevv <146669687+MaximNikolaevv@users.noreply.github.com>
2026-07-16 01:42:08 +0300

    Created URL functionality + basic backend server
    + Added npm packages

---
+++ frontend-part/pages/app.js
@@ -0,0 +1,28 @@
+import page from '../../node_modules/page/page.mjs';
+import { render } from '../../node_modules/lit-html/lit-html.js';
+import { HomePage } from './HomePage.js';
+import { AboutUs } from './aboutUs.js';
+
+const Main = document.querySelector("main");
+Main.id = "root";
+
+const root = document.getElementById("root");
+
+
+function UpdateCTX(ctx, next) {
+    ctx.render = (template) => renderer(template, root);
+    next()
+}
+
+
+function renderer(template, root) {
+    render(template, root);
+
+}
+
+
+page(UpdateCTX);
+page("/", HomePage);
+page("/aboutUs", AboutUs);
+
+page.start();
